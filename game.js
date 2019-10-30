799bd8a425d7e252cb20aedf8ed3feab45cca4f6
Besley Stephan <STEPHEKJ@mackillop.act.ecu.au>
2019-10-31 10:59:15 +1100

    Adding better code
    We fixed some movement controls and added a hit box for our player

--- game.js
+++ game.js
@@ -1,46 +1,52 @@
 var game = new Phaser.Game(800, 500, Phaser.CANVAS, 'Family Business', { preload: preload, create: create, update: update });
 
+var player
+var D
+var A
+var spacebar
+
 function preload() {
 
-  var Jossepi
-  var Alien
-  var D
-  var A
-  var Space
-
-  }
-
   game.load.image('bg', 'assets/Background.png');
-  game.load.spritesheet('Jossepi', 'assets/Jossepi.png', -, -);
-  game.load.spritesheet('Alien', 'assets/Alien.png', -, -);
+  game.load.spritesheet('Jossepi', 'assets/Jossepi.png', 750, 50);
+  game.load.spritesheet('Alien', 'assets/Alien.png', 750, 450);
 }
 
 function create() {
 
+  game.physics.startSystem(Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
+
+  player = game.add.sprite(48, game.world.height - 150, 'Jossepi');
+  game.physics.arcade.enable(player);
+
+  player.body.bounce.y = 0.5;
+  player.body.gravity.y = 250;
+  player.body.collideWorldBounds = true;
+
+  player.animations.add('left', [1, 2, 3, 4], 10, true);
+  player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+  spacebar = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+  D = game.input.keyboard.addKey(Phaser.KeyCode.D);
+  A = game.input.keyboard.addKey(Phaser.KeyCode.A);
 
 }
 
 function update() {
 
-  Jossepi.body.velocity.x = 0;
+  player.body.velocity.x = 0;
 
-  if (cursors.D.isDown){
+  if (D.isDown){
 
-    Jossepi.body.velocity.x = 150;
-    Jossepi.animations.play('right');
+    player.body.velocity.x = 150;
+    player.animations.play('right');
 
   }
 
-  else if (cursors.A.isDown){
+  else if (A.isDown){
 
-    Jossepi.body.velocity.x = -150;
-    Jossepi.animations.play('left');
-  }
-
-  else
-  {
-    Jossepi.animations.stop();
-    Jossepi.frame = 1;
+    player.body.velocity.x = -150;
+    player.animations.play('left');
   }
 }
