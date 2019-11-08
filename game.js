01f2bf59577f710fba41c3435c6596974a222002
Besley Stephan <STEPHEKJ@mackillop.act.ecu.au>
2019-11-08 13:57:18 +1100

    Fixed up some stuff

--- game.js
+++ game.js
@@ -1,15 +1,16 @@
-var game = new Phaser.Game(400, 200, Phaser.CANVAS, 'Family Business', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(400, 200, Phaser.CANVAS, 'Family Business', { preload: preload, create: create, update: update, render: render });
 
 var player
 var D
 var A
 var spacebar
 var jump
+var ground
 
 function preload() {
 
   game.load.image('bg', 'assets/Background.png');
-  game.load.image('ground', 'assets/Ground.png');
+  game.load.image('ground', 'assets/ground.png');
   game.load.spritesheet('player', 'assets/jossepi.png', 22, 36);
   game.load.spritesheet('alien', 'assets/Alien.png', 750, 450);
 }
@@ -19,13 +20,20 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
 
+  ground = game.add.sprite(0, 195, 'ground');
+  game.physics.arcade.enable(ground);
+  ground.body.immovable = true;
+
   player = game.add.sprite(48, game.world.height - 150, 'player');
   game.physics.arcade.enable(player);
   player.smoothed = false;
 
-  player.body.bounce.y = 0.5;
+  //player.body.bounce.y = 0.5;
   player.body.gravity.y = 250;
   player.body.collideWorldBounds = true;
+  player.onGround = false;
+  player.lastJump = 0;
+  player.canJump = true;
 
   player.animations.add('right', [0, 1, 2, 3], 10, true);
   player.animations.add('left', [4, 5, 6, 7], 10, true);
@@ -34,11 +42,11 @@ function create() {
   D = game.input.keyboard.addKey(Phaser.KeyCode.D);
   A = game.input.keyboard.addKey(Phaser.KeyCode.A);
 
-  jump = 0;
-
 }
 
 function update() {
+
+  game.physics.arcade.collide(player, ground, checkGroundCollision);
 
   player.body.velocity.x = 0;
 
@@ -57,13 +65,25 @@ function update() {
 
 console.log(jump);
 
-
-  if (jump < 2)
-  {
-    if (spacebar.isDown && player.body.velocity.y > -150 )
+  if (spacebar.isDown && player.onGround)
     {
-      jump = jump + 1;
+      player.onGround = false;
+      player.lastJump = game.time.totalElapsedSeconds;
       player.body.velocity.y = -175;
-    }
+      player.canJump = false;
+  } else if (spacebar.isDown && player.canJump) {
+      player.body.velocity.y += -175;
+      player.canJump = false;
   }
+}
+
+function checkGroundCollision(player, ground) {
+  player.onGround = player.body.touching.down;
+  player.canJump = true;
+}
+
+
+function render () {
+  game.debug.body(player);
+  game.debug.body(ground);
 }
