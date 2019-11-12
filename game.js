2c2e93e1681b6992250602574878584a378333b0
Besley Stephan <STEPHEKJ@mackillop.act.ecu.au>
2019-11-12 11:15:36 +1100

    We fixed some stuff

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 var game = new Phaser.Game(400, 200, Phaser.CANVAS, 'Family Business', { preload: preload, create: create, update: update });
 
-var player
-var D
-var A
-var W
-var S
+var player;
+var D;
+var A;
+var W;
+var S;
+var text;
+var text2;
 
 function preload() {
 
@@ -22,10 +24,23 @@ function create() {
   game.physics.arcade.enable(player);
   player.smoothed = false;
 
+  text = game.add.text(player.x + 15, player.y - 20, 'This is Jossepi\n           \u2193', {
+      fontSize: '12px',
+      fill: '#fff'
+    });
+
+  text2 = game.add.text(player.x + 15, player.y + 45, '(if that wasnt already obvious for you)', {
+        fontSize: '12px',
+        fill: '#fff'
+    });
+
+  text.anchor.setTo(0.5, 0.5);
+  text2.anchor.setTo(0.5, 0.5);
+
   player.body.collideWorldBounds = true;
 
-  player.animations.add('right', [0, 1, 2, 3], 10, true);
-  player.animations.add('left', [4, 5, 6, 7], 10, true);
+  player.animations.add('right', [0, 1, 2, 3], 4, true);
+  player.animations.add('left', [4, 5, 6, 7], 4, true);
 
   W = game.input.keyboard.addKey(Phaser.KeyCode.W);
   A = game.input.keyboard.addKey(Phaser.KeyCode.A);
@@ -37,11 +52,15 @@ function create() {
 function update() {
 
   player.body.velocity.x = 0;
+  player.body.velocity.y = 0;
+
+  var killText = false;
 
   if (D.isDown){
 
     player.body.velocity.x = 150;
     player.animations.play('right');
+    killText = true;
 
   }
 
@@ -49,18 +68,26 @@ function update() {
 
     player.body.velocity.x = -150;
     player.animations.play('left');
+    killText = true;
 
   }
 
-  else if (W.isDown){
+  if (W.isDown){
 
     player.body.velocity.y = -150;
+    killText = true;
 
   }
 
   else if (S.isDown){
 
     player.body.velocity.y = 150;
+    killText = true;
 
+  }
+
+  if (killText && text) {
+    text.destroy();
+    text2.destroy();
   }
 }
