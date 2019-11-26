4f5421b2886a2ba060087b86a40ccccd9dc966af
Besley Stephan <STEPHEKJ@mackillop.act.ecu.au>
2019-11-26 11:16:08 +1100

    oiuhuih

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(400, 200, Phaser.CANVAS, 'Family Business', { preload: preload, create: create, update: update });
 
 var player;
+var shotgun;
 var D;
 var A;
 var W;
@@ -12,7 +13,7 @@ function preload() {
 
   game.load.image('background', 'assets/Tester background.png');
   game.load.spritesheet('player', 'assets/jossepi.png', 22, 36);
-  game.load.spritesheet('alien', 'assets/Alien.png');
+  game.load.spritesheet('shotgun', 'assets/Shotgun.png', 64, 12);
 }
 
 function create() {
@@ -23,6 +24,9 @@ function create() {
   player = game.add.sprite(180, game.world.height - 75, 'player');
   game.physics.arcade.enable(player);
   player.smoothed = false;
+
+  shotgun = game.add.sprite(180, player.y + 20, 'shotgun');
+  game.physics.arcade.enable(shotgun);
 
   text = game.add.text(player.x + 15, player.y - 20, 'This is Jossepi\n           \u2193', {
       fontSize: '12px',
@@ -38,29 +42,44 @@ function create() {
   text2.anchor.setTo(0.5, 0.5);
 
   player.body.collideWorldBounds = true;
+  shotgun.body.collideWorldBounds = true;
 
   player.animations.add('right', [0, 1, 2, 3], 5, true);
   player.animations.add('left', [4, 5, 6, 7], 5, true);
+  shotgun.animations.add('shoot right', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
+  shotgun.animations.add('shoot left', [], 10, true);
 
   W = game.input.keyboard.addKey(Phaser.KeyCode.W);
   A = game.input.keyboard.addKey(Phaser.KeyCode.A);
   S = game.input.keyboard.addKey(Phaser.KeyCode.S);
   D = game.input.keyboard.addKey(Phaser.KeyCode.D);
 
-  game.add.image('background', 0, 0);
-
 }
 
 function update() {
 
+  shotgun.body.velocity.x = 0;
+  shotgun.body.velocity.x = 0;
   player.body.velocity.x = 0;
   player.body.velocity.y = 0;
 
   var killText = false;
 
+  if (cursors.right.isDown){
+
+    shotgun.animations.play('shoot right');
+
+  }
+
+  else if (cursors.left.isDown){
+
+    player.animations.play('left');
+  }
+
   if (D.isDown){
 
     player.body.velocity.x = 130;
+    shotgun.body.velocity.x = 130;
     player.animations.play('right');
     killText = true;
 
@@ -69,6 +88,7 @@ function update() {
   else if (A.isDown){
 
     player.body.velocity.x = -130;
+    shotgun.body.velocity.x = -130;
     player.animations.play('left');
     killText = true;
 
@@ -77,6 +97,7 @@ function update() {
   if (W.isDown){
 
     player.body.velocity.y = -130;
+    shotgun.body.velocity.y = -130;
     killText = true;
 
   }
@@ -84,6 +105,7 @@ function update() {
   else if (S.isDown){
 
     player.body.velocity.y = 130;
+    shotgun.body.velocity.y = 130;
     killText = true;
 
   }
